d4ecf7723af9beb71374ddd4f9e0dbb6838f7c87
Piotr Prus <prus.piotr@gmail.com>
2018-02-11 15:01:34 +0100

    Initial commit

---
+++ app/src/main/java/com/prosurpit/transportdisplay/DisplayActivity.java
@@ -0,0 +1,78 @@
+package com.prosurpit.transportdisplay;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+
+import java.util.HashMap;
+
+public class DisplayActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final String TAG = DisplayActivity.class.getSimpleName();
+    private HashMap<String, Marker> mMarkers = new HashMap<>();
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMaxZoomPreference(16);
+        loginToFirebase();
+    }
+
+    private void loginToFirebase() {
+        String email = BuildConfig.FIREBASE_LOGIN;
+        String password = BuildConfig.FIREBASE_PASSWORD;
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "Firebase auth success");
+                    subscribeToUpdates();
+                } else {
+                    Log.d(TAG, "Firebase auth failed");
+                }
+            }
+        });
+    }
+
+    private void subscribeToUpdates() {
+
+    }
+
+    private void setMarker(DataSnapshot dataSnapshot){
+
+    }
+}
